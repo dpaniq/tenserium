d8bffa796272a657851bd80560875b6373ba74e5
Bogdan Gromov <14brother@gmail.com>
2025-02-14 22:30:27 +0200

    tech: update title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'baam' title`, () => {
+  it(`should have the 'tenserium' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('baam');
+    expect(app.title).toEqual('tenserium');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, baam');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, tenserium',
+    );
   });
 });
